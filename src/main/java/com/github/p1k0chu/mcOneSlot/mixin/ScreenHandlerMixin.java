183680d479c69fd3363027bf94b77979ee0730c9
p1k0chu <anatoliyid0207@gmail.com>
2025-04-17 16:26:11 +0300

    Disable shift click
    its not that useful when you only have one slot,
    and too annoying to fix

--- src/main/java/com/github/p1k0chu/mcOneSlot/mixin/ScreenHandlerMixin.java
+++ src/main/java/com/github/p1k0chu/mcOneSlot/mixin/ScreenHandlerMixin.java
@@ -1,9 +1,13 @@
 package com.github.p1k0chu.mcOneSlot.mixin;
 
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.slot.SlotActionType;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ScreenHandler.class)
 public class ScreenHandlerMixin {
@@ -14,5 +18,12 @@ public class ScreenHandlerMixin {
     @ModifyConstant(method = "addPlayerHotbarSlots", constant = @Constant(intValue = 9))
     int changeHotbarSize(int constant) {
         return 1;
+    }
+
+    @Inject(method = "internalOnSlotClick", at = @At("HEAD"), cancellable = true)
+    void internalOnSlotClick(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
+        if(actionType == SlotActionType.QUICK_MOVE) {
+            ci.cancel();
+        }
     }
 }
